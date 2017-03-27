56d606ff471845a4c9cdf12dcc97f90080dc6a3f
Cody Brant <codyjbra@gmail.com>
2017-03-27 19:47:03 -0400

    created options menu

--- app/src/main/java/org/fountainhook/currencies/MainActivity.java
+++ app/src/main/java/org/fountainhook/currencies/MainActivity.java
@@ -64,17 +64,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id= item.getItemId();
+        switch(id)
+        {
+            case R.id.mnu_invert:
+                //TO DO
+                break;
+            case R.id.mnu_codes:
+                //TO DO
+                break;
+            case R.id.nu_exit:
+                finish();
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 }
