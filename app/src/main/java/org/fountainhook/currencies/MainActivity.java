bca4a100a9722f28c108965584db1707e515f342
Cody Brant <codyjbra@gmail.com>
2017-03-27 19:56:58 -0400

    implemented options menu behaviour; modifies manifest file

--- app/src/main/java/org/fountainhook/currencies/MainActivity.java
+++ app/src/main/java/org/fountainhook/currencies/MainActivity.java
@@ -1,5 +1,10 @@
 package org.fountainhook.currencies;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -56,6 +61,42 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public boolean isOnline()
+    {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnectedOrConnecting())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void launchBrowser(String strUri)
+    {
+        if (isOnline())
+        {
+            Uri uri = Uri.parse(strUri);
+            //call implicit intent
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        }
+    }
+
+    private void invertCurrencies()
+    {
+        int nFor = mForSpinner.getSelectedItemPosition();
+        int nHom = mHomSpinner.getSelectedItemPosition();
+
+        mForSpinner.setSelection(nHom);
+        mHomSpinner.setSelection(nFor);
+
+        mConvertedTextView.setText("");
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -70,12 +111,12 @@ public class MainActivity extends AppCompatActivity {
         switch(id)
         {
             case R.id.mnu_invert:
-                //TO DO
+                invertCurrencies();
                 break;
             case R.id.mnu_codes:
-                //TO DO
+                launchBrowser(SplashActivity.URL_CODES);
                 break;
-            case R.id.nu_exit:
+            case R.id.mnu_exit:
                 finish();
                 break;
         }
